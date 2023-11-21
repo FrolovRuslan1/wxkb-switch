e351a10ff9f693ff125215ed115fc301b1eeb2f3
Ruslan <gald.rus@yandex.ru>
2023-11-21 10:18:45 +0300

    start

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# wxkb-switch
-wxkb-switch is a C program that allows to query and change the XKB layout state on Wyaland.
+# wxkb_switch
+wxkb_switch is a C program that allows to query and change the XKB layout state on Wyaland.
---
+++ src/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
+#include <getopt.h>
+#include <pwd.h>
+
+
+#include <X11/Xlib.h>
+#include <X11/extensions/XTest.h>
+#include <X11/X.h>
+#include <X11/Xatom.h>
+#include <X11/keysym.h>
+
+#include <X11/XKBlib.h>
+
+#include <xkbcommon/xkbcommon.h>
+#include <xkbcommon/xkbcommon-x11.h>
+
+#include <sys/epoll.h>
+#include <sys/types.h>
+
+#include <config.h>
+#include <libconfig.h>
+
+
+
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
